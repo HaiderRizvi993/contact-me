c7372d19fde7b1112adc79e08f58baee2510e4cd
HaiderRizvii <sha.rizvi883@gmail.com>
2021-06-10 00:35:12 +0500

    Client added

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import {Route, Switch} from 'react-router-dom';
+import './App.css';
+
+import Landing from './components/layout/Landing';
+import Landing from './components/layout/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,30 @@
+console.clear();
+const express = require('express');
+
+const connectDB = require('./config/db');
+
+const userRoute = require('./routes/api/users');
+const profileRoute = require('./routes/api/profile');
+const postRoute = require('./routes/api/posts');
+const authRoute = require('./routes/api/auth');
+
+const app = express();
+
+//DB conneaction
+connectDB();
+
+//Middlewares
+app.use(express.json({extended : false}));
+
+app.get('/', (req, res) => res.send('API is running!!!'))
+
+//Defined Routes
+app.use('/api/users', userRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/auth', authRoute);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log('App listening on port 4000!');
+});
